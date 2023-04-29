8cbe96c750ad249eda1cdc397a3b63bfce617590
ktox <ktoxsyrovv@inbox.ru>
2023-04-29 21:46:08 +0300

    redesign

--- app/src/main/java/com/example/diplom/RaceListAdapter.kt
+++ app/src/main/java/com/example/diplom/RaceListAdapter.kt
@@ -27,7 +27,7 @@ class RaceListAdapter(private val context: Context) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RaceViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.race_item,
+            R.layout.new_race_item,
             parent,
             false
         )
